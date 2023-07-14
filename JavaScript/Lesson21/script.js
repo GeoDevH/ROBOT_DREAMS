01688ee650f64b33e0e64a814e8692aa0e218521
GeoDevH <g.voloshin22@gmail.com>
2023-07-14 11:22:17 +0300

    Lesson21.1

--- JavaScript/Lesson21/script.js
+++ JavaScript/Lesson21/script.js
@@ -3,8 +3,8 @@ console.log ('hello world!');
 //let var1 = Number(prompt('Enter your first digit:', 0));
 //let var2 = Number(prompt('Enter your second digit:', 0));
 
-let var1 = +prompt ('Enter your first digit: ', 0);
-let var2 = +prompt ('Enter your second digit: ', 0);
+let var1 = +prompt('Enter your first digit: ', 0);
+let var2 = +prompt('Enter your second digit: ', 0);
 
 alert('Sum = ' + (var1 + var2) + '\nDifference = ' + (var1 - var2) + '\nAddition = ' + (var1 * var2) + '\nDivision = ' + (var1 / var2));
 
