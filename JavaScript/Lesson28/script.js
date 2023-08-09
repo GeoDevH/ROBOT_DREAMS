668e6baac66065d14b69b05a4887adfd84663b77
GeoDevH <g.voloshin22@gmail.com>
2023-08-09 08:20:34 +0300

    Lesson28.1

--- JavaScript/Lesson28/script.js
+++ JavaScript/Lesson28/script.js
@@ -2,19 +2,19 @@ console.log ('Hello world!');
 
 let zoomPhotos = document.querySelectorAll('.zoom_photo');
 
-  function zoomClick(photo) {
-    if (!photo.classList.contains('animation_zoom')) {
-      if (photo.classList.contains('gallery_photo_zoom')) {
-          photo.style.transform = 'scale(1.5)';
-      } else if (photo.classList.contains('home_photo_zoom')) {
-          photo.style.transform = 'scale(1.1)';
-      }
-      photo.classList.add('animation_zoom');
-    } else {
-      photo.style.transform = 'scale(1)';
-      photo.classList.remove('animation_zoom');
+function zoomClick(photo) {
+  if (!photo.classList.contains('animation_zoom')) {
+    if (photo.classList.contains('gallery_photo_zoom')) {
+        photo.style.transform = 'scale(1.5)';
+    } else if (photo.classList.contains('home_photo_zoom')) {
+        photo.style.transform = 'scale(1.1)';
     }
+    photo.classList.add('animation_zoom');
+  } else {
+    photo.style.transform = 'scale(1)';
+    photo.classList.remove('animation_zoom');
   }
+}
 
   zoomPhotos.forEach(function (photo) {
     photo.addEventListener('click', function () {
